9b733ed794cb790edf464552b548f6f987e4b5a5
Vasin Max Limsukhawat <max.limsukhawat@qsc.com>
2016-05-15 21:41:39 -0600

    asds

--- index.js
+++ index.js
@@ -176,7 +176,7 @@ function isLoggedIn(req, res, next) {
         return next();
 
     // if they aren't redirect them to the home page
-    res.redirect('/login');
+    res.redirect('/vocab/login');
 }
 
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
